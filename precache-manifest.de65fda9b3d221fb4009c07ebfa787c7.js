548143f768880fc3e1f18dd8f6826c42624b0ca6
unknown <arturbohanov1908@gmail.com>
2020-03-17 19:11:56 +0200

    Updates

---
+++ precache-manifest.de65fda9b3d221fb4009c07ebfa787c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177ebc77a3cc5aef697d5d81ce077987",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "bafe8be2a5437275307e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a51cb528.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.a51cb528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3bbd4c65f8d071ba0bd",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.ff293467.chunk.js"
+  },
+  {
+    "revision": "238a1528aed3a14ce705",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d660e622.js"
+  }
+]);
